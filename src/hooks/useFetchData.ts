04bc59af0fc0277819f2ec8e58f11cdacb944862
Rico Putra Pradana <pradanaricoputra@gmail.com>
2023-09-23 22:10:57 +0700

    Implement debounce for the searching

--- src/hooks/useFetchData.ts
+++ src/hooks/useFetchData.ts
@@ -2,19 +2,27 @@ import { ProductResponse } from "@/pages/api/product";
 import { ActionType } from "@/store/types";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
+import { useDebounce } from "use-debounce";
 import useProductContext from "./useProductContext"
 
 const useFetchData = () => {
+  const { basePath } = useRouter();
+
   const {
     state: { search },
     dispatch
   } = useProductContext();
-  const { basePath } = useRouter();
+
+  /**
+   * `searchValue` will be updated after 0.5 second
+   * after the user typed the last character
+   */
+  const [searchValue] = useDebounce(search, 500);
 
   useEffect(() => {
     let URL = basePath + "/api/product";
     
-    if (search) URL += `?search=${search}`;
+    if (searchValue) URL += `?search=${searchValue}`;
 
     fetch(URL)
       .then((res) => res.json())
@@ -27,7 +35,7 @@ const useFetchData = () => {
       .catch((error) => {
         alert(`Failed fetch data. Error: ${error}`);
       });
-  }, [search]);
+  }, [searchValue]);
 }
 
 export default useFetchData
